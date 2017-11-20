00d52adaae247803e313026c41456565e4bcbfb3
Brad Hambrice <hambrice@uab.edu>
2017-11-20 23:32:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,11 @@ function forLoop (array) {
   }
 }
 }
+
+`string text ${expression} string text`
 function forLoop (array) {
   for (let i=0; i<25; i++) {
-    array.push("I am ${i} strange loop${i===0 ? '' : 's'}.")
+    array.push(`I am ${i} strange loop${i===0 ? '' : 's'}.`)
     return array
   }
 }
