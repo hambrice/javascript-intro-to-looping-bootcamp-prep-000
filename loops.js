5c807cd4931998e39e3e00aa693c3925a2ec8f2e
Brad Hambrice <hambrice@uab.edu>
2017-11-20 23:15:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,4 +10,10 @@ function forLoop (array) {
   }
 }
 }
-console.log [array];
+function forLoop (array) {
+  for (let i=0; i<25; i++) {
+  array.push("I am ${i} strange loop${i===0 ? '' : 's'}.")
+  return array
+  }
+}
+}
