be6de1f04eb2f6ed463909ae8124a5058c1b1428
Brad Hambrice <hambrice@uab.edu>
2017-11-20 22:52:41 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 for (var i=0; i<25; i++) {
-  if (var i = 1) {
-    return I am 1 strange loop.
+  if (i == 1) {
+    return 'I am 1 strange loop.';
   } else {
-    return I am ${i} strange loops.
+    return 'I am ${i} strange loops.';
+  }
 }
