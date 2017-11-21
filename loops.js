e1ca11833bff76bf85e8e7d38af55871be200aa0
Brad Hambrice <hambrice@uab.edu>
2017-11-21 00:07:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,7 +17,7 @@ function doWhileLoop (array) {
     return Math.random() >= 0.5;
   }
   do {
-    array.length - 1;
+    array = array.length - 1;
   } while (array.length > 0 && maybeTrue());
   return array
 }
